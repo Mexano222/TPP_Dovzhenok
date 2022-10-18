5ca5907e70f8c10931ad5c917f485eaeafb81abb
mexano222 <mexano222@gmail.com>
2022-10-18 19:41:06 +0300

    small corrections

--- src/stu/lab1/TestRunner.java
+++ src/stu/lab1/TestRunner.java
@@ -2,6 +2,7 @@ package stu.lab1;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 
 public class TestRunner {
 
@@ -14,21 +15,26 @@ public class TestRunner {
         //Get all methods of given class
         Method[] testMethods = testClass.getDeclaredMethods();
         //Loop through all methods of given class
-        int totalTests = 0, totalSuccessful = 0, totalFailed = 0;
+        int totalSuccessful = 0, totalFailed = 0;
+
         for (Method method : testMethods) {
             //Checking if the method meets the requirements
             if (!isTestMethod(method)) {
                 System.out.printf("\tMethod: %s is not test method\n", method.getName());
                 continue;
             }
+
             //If method meets the requirements invoke test
-            totalTests++;
             if (invokeTestMethod(method))
                 totalSuccessful++;
             else
                 totalFailed++;
+
         }
-        System.out.printf("\tTotal methods: %d Total tests: %d Successful tests: %d Failed tests: %d\n", testMethods.length, totalTests, totalSuccessful, totalFailed);
+        System.out.printf("\tTotal methods: %d Total tests: %d Successful tests: %d Failed tests: %d\n",
+                testMethods.length,
+                (int) Arrays.stream(testMethods).filter(TestRunner::isTestMethod).count(),
+                totalSuccessful, totalFailed);
     }
 
     //Check whether the given method is a test method or a regular one
@@ -41,15 +47,13 @@ public class TestRunner {
 
     //Creates an instance of the given class and invokes the method
     private static boolean invokeTestMethod(Method method) {
-        String result = "\tTest: " + method.getName() + " ";
+        String testName = "\tTest: " + method.getName() + " ";
         try {
             method.invoke(method.getDeclaringClass().getDeclaredConstructor().newInstance());
-            result += "SUCCESSFUL";
-            System.out.println(result);
+            System.out.println(testName + "SUCCESSFUL");
             return true;
         } catch (Exception e) {
-            result += "FAILED, error: " + e.getCause().getMessage();
-            System.out.println(result);
+            System.out.println(testName + "FAILED, error: " + e.getCause().getMessage());
             return false;
         }
     }
